6cd118ca7fdfab43791211162fa9fd4104541e5c
gymcoding <bruce.lean17@gmail.com>
2024-03-27 15:33:59 +0900

    25_이벤트 전파 막기

--- src/main.js
+++ src/main.js
@@ -5,12 +5,21 @@ function toggleMenu() {
 
 function init() {
   const $navToggle = document.getElementById('nav-toggle');
-  $navToggle.addEventListener('click', () => {
+  $navToggle.addEventListener('click', (e) => {
+    e.stopPropagation();
     toggleMenu();
   });
 
   const $navLinkList = document.querySelectorAll('.nav__link');
   $navLinkList.forEach((n) => n.addEventListener('click', toggleMenu));
+
+  const $body = document.querySelector('body');
+  $body.addEventListener('click', () => {
+    const $navMenu = document.getElementById('nav-menu');
+    if ($navMenu.classList.contains('show')) {
+      $navMenu.classList.remove('show');
+    }
+  });
 }
 
 init();
